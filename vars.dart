519b8aefa27b83a5a74de5a184b9576ebde62c3a
Takeshi Kondo <10370988+chaspy@users.noreply.github.com>
2024-12-15 20:20:10 +0900

    feat: Add hello and vars Dart files

---
+++ vars.dart
@@ -0,0 +1,11 @@
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+
+void main() {
+  print(name);
+  print(year);
+  print(antennaDiameter);
+  print(flybyObjects);
+}
